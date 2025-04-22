c21e0565a5b46de54314cba7c8c9dab8a1b149cf
rayen <rayenn38@gmail.com>
2025-04-22 22:20:57 +0100

    feat(auth): add routes for email verification and token handling

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'verify-email/:token', component: VerifyEmailComponent },
 ];
 
 @NgModule({
