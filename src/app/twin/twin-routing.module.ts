ce107048b6a215a5ac608d35d2302be4337199e7
rayen <rayenn38@gmail.com>
2025-04-24 08:50:27 +0100

    feat(routing): add routing module for Twin components with routes for listing, creating, and detail views

---
+++ src/app/twin/twin-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateTwinComponent } from './components/create-twin/create-twin.component';
+import { ListTwinComponent } from './components/list-twin/list-twin.component';
+import { DetailTwinComponent } from './components/detail-twin/detail-twin.component';
+
+const routes: Routes = [
+  { path: '', component: ListTwinComponent },
+  { path: 'create', component: CreateTwinComponent },
+  { path: ':id', component: DetailTwinComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
