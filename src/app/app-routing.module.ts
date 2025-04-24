d93da1c11f0ef6c4aab0215f578539cabf200dac
rayen <rayenn38@gmail.com>
2025-04-24 08:54:42 +0100

    feat(routing): add route for Twin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomePageComponent } from './shared/components/homepage/homepage.compone
 const routes: Routes = [
   // { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'twin', loadChildren: () => import('./twin/twin.module').then(m => m.TwinModule) },
   { path: 'home', component:HomePageComponent},
 ];
 
