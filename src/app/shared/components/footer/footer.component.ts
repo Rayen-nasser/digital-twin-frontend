230c3aa701e87b20242bc5255e2bd6f72d9eb85e
rayen <111684397+Rayen-nasser@users.noreply.github.com>
2025-05-08 11:04:17 +0100

    feat(footer): implement newsletter subscription form with reactive forms and toastr notifications

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,26 +1,33 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedService } from '../../service/shared.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
   newsletterForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private shredService: SharedService,
+    private toasterService: ToastrService
+  ) {
     this.newsletterForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
   onSubscribe() {
     if (this.newsletterForm.valid) {
-      // Handle subscription logic here
-      console.log('Subscribe:', this.newsletterForm.value);
-      this.newsletterForm.reset();
+      this.shredService.addSubscribe(this.newsletterForm.value.email).subscribe((res:any) =>{
+        this.newsletterForm.reset();
+        this.toasterService.success('Subscribed successfully');
+      });
     }
   }
 }
