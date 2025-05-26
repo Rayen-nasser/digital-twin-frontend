6368dea79bb7bd78c233f84aa92a660ff7f1396f
rayen <111684397+Rayen-nasser@users.noreply.github.com>
2025-05-26 17:48:06 +0100

    fix(environment): update API URL to correct port for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/v1',
+  apiUrl: 'http://127.0.0.1:8001/api/v1',
   streamingServiceUrl: 'http://localhost:3001/api/streaming',
   appName: 'Digital Twin'
 };
